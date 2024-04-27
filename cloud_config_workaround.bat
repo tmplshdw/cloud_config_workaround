c83cef270f90bbd8cc9cfccd2f89b78ef8156b5d
tmplshdw <107013028+tmplshdw@users.noreply.github.com>
2024-04-27 11:20:06 -0700

    paths file now uses Win path vars, switch to robocopy in bat, tested with Monkey Isl

--- cloud_config_workaround.bat
+++ cloud_config_workaround.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal
-set "SteamAppId=1687950"
+
+set "scriptpath=%~dp0"
 
 :: get Documents folder from registry ( will work even with onedrive )
 for /f "tokens=2*" %%a in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Personal') do set DOCUMENTS=%%b
@@ -22,15 +23,12 @@ mkdir "%GOODCONFIGSDIR%\%SteamAppId%"
 for /f "delims=" %%a in ('powershell.exe -command "$result=%STEAMID%-76561197960265728; Write-Output $result"') do set SteamID3=%%a
 
 :: get location of config file from the paths.txt file based on steam appid
-for /f "tokens=2 delims=;" %%F in ('findstr %SteamAppId% paths.txt') do set "CONFIGPATH=%%F"
+for /f "tokens=2 delims=;" %%F in ('findstr %SteamAppId% %scriptpath%paths.txt') do set "CONFIGPATH=%%F"
+call set "CONFIGPATH=%CONFIGPATH%"
 
-:: get just the config file name
-for /f "delims=|" %%F in ("%CONFIGPATH%") do set "GAMECONFIG=%%~nxF"
-
-if defined CONFIGPATH (echo %CONFIGPATH%)
-if defined GAMECONFIG (echo %GAMECONFIG%)
-pause
-exit
+:: get the config file name
+for /f "tokens=3 delims=;" %%F in ('findstr %SteamAppId% %scriptpath%paths.txt') do set "GAMECONFIG=%%F"
+call set "GAMECONFIG=%GAMECONFIG%"
 
 :: if this is defined then it's a game in the list so use the workaround
 if defined GAMECONFIG (goto workaround)
@@ -40,9 +38,9 @@ exit
 
 :workaround
 :: copy the wanted config file to the location used by game
-copy "%GOODCONFIGSDIR%\%SteamAppId%\%GAMECONFIG%" "%CONFIGPATH%"
+robocopy "%GOODCONFIGSDIR%\%SteamAppId%" "%CONFIGPATH%" "%GAMECONFIG%" /V 
 :: this is %command% (i.e. the game exe) and any parameters from the launch options
 %*
 :: save any config changes you made in-game for next time
-copy "%CONFIGPATH%" "%GOODCONFIGSDIR%\%SteamAppId%\"
+robocopy "%CONFIGPATH%" "%GOODCONFIGSDIR%\%SteamAppId%" "%GAMECONFIG%" /V
 exit
--- cloud_config_workaround.sh
+++ cloud_config_workaround.sh
@@ -2,6 +2,8 @@
 
 # location to keep good local config files
 GOOD_CONFIGS_PATH="/home/${USER}/Documents/game_configs"
+# location of paths.txt file containing the config paths for games, default in script directory
+PATHS_FILE="$(dirname $0)/paths.txt"
 LOGFILE="${GOOD_CONFIGS_PATH}/${SteamAppId}_config_workaround.log"
 # user directory in the proton prefix
 WIN_USER_PATH="pfx/drive_c/users/steamuser"
@@ -16,16 +18,25 @@ STEAMID=$(grep -Pzo '"'${SteamUser}'"\s+{\s+"SteamID"\s+"[0-9]+"' /home/${USER}/
 SteamID3=$((${STEAMID}-76561197960265728))
 
 # get the path of config from the paths.txt file
-CONFIG_PATH=$(grep ${SteamAppId} "$(dirname $0)/paths.txt" | cut --delimiter=';' --fields=2 -)
-CONFIG=${CONFIG_PATH##*/} #https://stackoverflow.com/a/23497364
+CONFIG_PATH=$(grep ${SteamAppId} ${PATHS_FILE} | cut --delimiter=';' --fields=2 -)
+CONFIG=$(grep ${SteamAppId} ${PATHS_FILE} | cut --delimiter=';' --fields=3 -)
+
+# replace Windows path variables with their equivalent for Proton prefix
+CONFIG_PATH=$(echo ${CONFIG_PATH} | sed \
+    -e 's/%APPDATA%/Application Data/'\
+    -e 's/%DOCUMENTS%/Documents/'\
+    -e 's/%USERPROFILE%//'\
+    -e 's/%LOCALAPPDATA%/AppData\/Local/'\
+    -e 's/%STEAMID%/\${STEAMID}/'\
+    -e 's/%SteamID3%/\${SteamID3}/'\
+)
 
 # STEAM_COMPAT_DATA_PATH is set by Steam to be the location for the prefix used by the game
 # by default ~/.local/share/Steam/steamapps/compatdata/$SteamAppId
 # but may be elsewhere depending on how you set up your steam library storage
 GAME_CONFIG_PATH="${STEAM_COMPAT_DATA_PATH}/${WIN_USER_PATH}/${CONFIG_PATH}"
-echo "game config file: ${GAME_CONFIG_PATH}" >> ${LOGFILE} 2>&1
 
-if [ "${CONFIG}" = "error" ]; then # run the game without workaround
+if [ -z ${CONFIG} ]; then # run the game without workaround
     echo "error" >> ${LOGFILE} 2>&1
     "$@" # filled in with %command% (game executable stuff) and any other launch options
 else
@@ -35,5 +46,5 @@ else
     "$@" # filled in with %command% (game executable stuff) and any other launch options
 
     # save any config changes you made in-game for next time
-    cp -v "${GAME_CONFIG_PATH}" "${GOOD_CONFIGS_PATH}/${SteamAppId}/" >> "${LOGFILE}" 2>&1
+    cp -v "${GAME_CONFIG_PATH}/${CONFIG}" "${GOOD_CONFIGS_PATH}/${SteamAppId}/" >> "${LOGFILE}" 2>&1
 fi
