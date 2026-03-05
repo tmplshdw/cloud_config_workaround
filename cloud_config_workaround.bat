5cbba4f7bb8bc85777579cacb30da5fe7f361a63
matthus <matthus@protonmail.ch>
2026-03-05 02:20:48 +0100

    New TheWitcher3 Launcher Patch Bat File

--- cloud_config_workaround.bat
+++ cloud_config_workaround.bat
@@ -18,6 +18,52 @@ for /f "tokens=*" %%a in (steam_appid.txt) do set "SteamAppId=%%a"
 :: Reparse the variable
 call set "SteamAppId=%SteamAppId%"
 
+@echo off
+setlocal enabledelayedexpansion
+
+if "%SteamAppId%"=="292030" (
+    set "GAMEEXE=witcher3.exe"
+    set "GAMEDIR=bin\x64_dx12"
+
+    if exist "launcher-configuration.json" (
+        set "FALLBACK_RAW="
+        for /f "tokens=2 delims=:" %%A in ('type "launcher-configuration.json" 2^>nul ^| find /i "fallback"') do (
+            set "FALLBACK_RAW=%%A"
+        )
+
+        if not "!FALLBACK_RAW!"=="" (
+            set "FALLBACK=!FALLBACK_RAW:,=!"
+            set "FALLBACK=!FALLBACK:"=!"
+            for /f "tokens=*" %%B in ("!FALLBACK!") do set "FALLBACK=%%B"
+            
+            if /i "!FALLBACK!"=="DirectX 11" set "GAMEDIR=bin\x64"
+            if /i "!FALLBACK!"=="DirectX 12" set "GAMEDIR=bin\x64_dx12"
+        )
+    )
+)
+
+if "%SteamAppId%"=="499450" (
+    set "GAMEEXE=witcher3.exe"
+    set "GAMEDIR=bin\x64_dx12"
+
+    if exist "launcher-configuration.json" (
+        set "FALLBACK_RAW="
+        for /f "tokens=2 delims=:" %%A in ('type "launcher-configuration.json" 2^>nul ^| find /i "fallback"') do (
+            set "FALLBACK_RAW=%%A"
+        )
+
+        if not "!FALLBACK_RAW!"=="" (
+            set "FALLBACK=!FALLBACK_RAW:,=!"
+            set "FALLBACK=!FALLBACK:"=!"
+            for /f "tokens=*" %%B in ("!FALLBACK!") do set "FALLBACK=%%B"
+            
+            if /i "!FALLBACK!"=="DirectX 11" set "GAMEDIR=bin\x64"
+            if /i "!FALLBACK!"=="DirectX 12" set "GAMEDIR=bin\x64_dx12"
+        )
+    )
+)
+
+
 :: location to keep good local config files
 set "GOODCONFIGSDIR=%DOCUMENTS%\game_configs"
 
@@ -35,15 +81,21 @@ call set "GAMECONFIG=%GAMECONFIG%"
 :: if this is defined then it's a game in the list so use the workaround
 if defined GAMECONFIG (goto workaround)
 :: else just run the game
-%*
-exit
+:: %*
+:: exit
 
 :workaround
 :: copy the wanted config file to the location used by game
 copy "%GOODCONFIGSDIR%\%SteamAppId%\%GAMECONFIG%" "%CONFIGPATH%" > "%GOODCONFIGSDIR%\%SteamAppId%_config_workaround.log"
-:: this is %command% (i.e. the game exe) and any parameters from the launch options
-%*
+
+:: Check if we defined a custom exe/dir (like for Witcher 3), otherwise run default
+if defined GAMEEXE (
+    start /wait "" /d "%GAMEDIR%" "%GAMEEXE%"
+) else (
+    %*
+)
+
 :: save any config changes you made in-game for next time
 copy "%CONFIGPATH%\%GAMECONFIG%" "%GOODCONFIGSDIR%\%SteamAppId%" >> "%GOODCONFIGSDIR%\%SteamAppId%_config_workaround.log"
 
-exit
+:: exit
