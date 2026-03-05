765eb5aa3714753fa4c1f8d41194ad2b461fe8b6
matthus <matthus@protonmail.ch>
2026-03-05 01:15:09 +0100

    MacOS Crossover Compatible bat script file

--- cloud_config_workaround.bat
+++ cloud_config_workaround.bat
@@ -3,7 +3,7 @@ setlocal
 
 set "scriptpath=%~dp0"
 
-:: get Documents folder from registry ( will work even with onedrive )
+:: get Documents folder from registry (will work even with OneDrive)
 for /f "tokens=2*" %%a in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Personal') do set DOCUMENTS=%%b
 :: Reparse the variable
 call set "DOCUMENTS=%DOCUMENTS%"
@@ -13,14 +13,16 @@ for /f "tokens=2*" %%a in ('reg query "HKEY_CURRENT_USER\Software\Microsoft\Wind
 :: Reparse the variable
 call set "APPDATA=%APPDATA%"
 
+:: get SteamAppId from steam_appid.txt
+for /f "tokens=*" %%a in (steam_appid.txt) do set "SteamAppId=%%a"
+:: Reparse the variable
+call set "SteamAppId=%SteamAppId%"
+
 :: location to keep good local config files
 set "GOODCONFIGSDIR=%DOCUMENTS%\game_configs"
 
 :: create a directory to keep good config file
 mkdir "%GOODCONFIGSDIR%\%SteamAppId%"
-
-:: get SteamID3 version by converting 64 Bit SteamID with powershell
-for /f "delims=" %%a in ('powershell.exe -command "$result=%STEAMID%-76561197960265728; Write-Output $result"') do set SteamID3=%%a
 
 :: get location of config file from the paths.txt file based on steam appid
 for /f "tokens=2 delims=;" %%F in ('findstr %SteamAppId% %scriptpath%paths.txt') do set "CONFIGPATH=%%F"
@@ -38,10 +40,10 @@ exit
 
 :workaround
 :: copy the wanted config file to the location used by game
-robocopy "%GOODCONFIGSDIR%\%SteamAppId%" "%CONFIGPATH%" "%GAMECONFIG%" /LOG:%GOODCONFIGSDIR%\%SteamAppId%_config_workaround.log
+copy "%GOODCONFIGSDIR%\%SteamAppId%\%GAMECONFIG%" "%CONFIGPATH%" > "%GOODCONFIGSDIR%\%SteamAppId%_config_workaround.log"
 :: this is %command% (i.e. the game exe) and any parameters from the launch options
 %*
 :: save any config changes you made in-game for next time
-robocopy "%CONFIGPATH%" "%GOODCONFIGSDIR%\%SteamAppId%" "%GAMECONFIG%" /LOG+:%GOODCONFIGSDIR%\%SteamAppId%_config_workaround.log
+copy "%CONFIGPATH%\%GAMECONFIG%" "%GOODCONFIGSDIR%\%SteamAppId%" >> "%GOODCONFIGSDIR%\%SteamAppId%_config_workaround.log"
 
 exit
